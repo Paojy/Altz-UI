0ae6c32816ef3e69901d85f0b0cc302bed32ca35
Paojy <359661355@qq.com>
2012-08-20 16:21:41 +0800

    UI fade toggle

--- Interface/AddOns/aCore/lib/fader.lua
+++ Interface/AddOns/aCore/lib/fader.lua
@@ -5,7 +5,6 @@
   -----------------------------
   -- GLOBAL FUNCTIONS
   -----------------------------
-
   --add some global functions
   local defaultFadeIn   = {time = 0.4, alpha = 1}
   local defaultFadeOut  = {time = 0.4, alpha = 0}
@@ -67,6 +66,7 @@
 
   -- Convenience function to do a simple fade out
   local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
+  	if aCoreCDB == nil or not aCoreCDB.fade then return end
     local fadeInfo = {};
     fadeInfo.mode = "OUT";
     fadeInfo.timeToFade = timeToFade;
@@ -80,13 +80,11 @@
 -----------------------
 
   --rButtonBarFader func
-  function rButtonBarFader(frame,buttonList,fadeIn,fadeOut)
-    --if 1 == 1 then return end
+  function ActionbarFader(frame,buttonList,fadeIn,fadeOut)
     if not frame or not buttonList then return end
     if not fadeIn then fadeIn = defaultFadeIn end
     if not fadeOut then fadeOut = defaultFadeOut end
     frame:EnableMouse(true)
-	-- dont fade by mouse conditions are met
 	
 	frame:SetScript("OnEnter", function(self) if frame.eventmode ~= 1 then UIFrameFadeIn(frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
     frame:SetScript("OnLeave", function(self) if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
@@ -221,41 +219,3 @@ function FrameEventFader(frame,fadeIn,fadeOut)
 	
     UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha)
 end
-
-
--- test frame
---[[
-
-local aMedia = "Interface\\Addons\\aMedia\\"
-local blank = "Interface\\Buttons\\WHITE8x8"
-
-local frameBD = {
-    edgeFile = aMedia.."grow", edgeSize = 3,
-    bgFile = blank,
-    insets = {left = 3, right = 3, top = 3, bottom = 3}
-}
-
-local function createBD(f, r, g, b, a, ba)
-	if f.border then return end
-	local border = CreateFrame("Frame", nil ,f)
-	border:SetPoint("TOPLEFT", -3, 3)
-	border:SetPoint("BOTTOMRIGHT", 3, -3)
-    border:SetBackdrop(frameBD)
-    border:SetBackdropColor(r, g, b, a)
-    border:SetBackdropBorderColor(0, 0, 0, ba)
-	if f:GetFrameLevel() - 1 >= 0 then
-		border:SetFrameLevel(f:GetFrameLevel() - 1)
-	else
-		border:SetFrameLevel(0)
-	end
-	f.border = border
-end
-
-
-local abtoggle = CreateFrame("Frame", nil, UIParent)
-createBD(abtoggle, 0, 0, 0, 1, 1)
-abtoggle:SetSize(50,50)
-abtoggle:SetPoint('CENTER')
-
-FrameEventFader(abtoggle)
-]]
