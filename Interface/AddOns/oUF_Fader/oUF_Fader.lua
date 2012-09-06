79f2c6bdf020c5e48c35a6f7ef18036e62d71017
Paojy <359661355@qq.com>
2012-09-06 16:07:13 +0800

    code clean

--- Interface/AddOns/oUF_Fader/oUF_Fader.lua
+++ Interface/AddOns/oUF_Fader/oUF_Fader.lua
@@ -2,69 +2,54 @@ local _, ns = ...
 local oUF = ns.oUF or oUF
 assert(oUF, 'oUF Fader was unable to locate oUF install')
 
-  local frameFadeManager = CreateFrame("FRAME")
-  
-  local function UIFrameFade(frame, fadeInfo)
-    if (not frame) then
-      return;
-    end
-    if ( not fadeInfo.mode ) then
-      fadeInfo.mode = "IN";
-    end
-    local alpha;
-    if ( fadeInfo.mode == "IN" ) then
-      if ( not fadeInfo.startAlpha ) then
-        fadeInfo.startAlpha = 0;
-      end
-      if ( not fadeInfo.endAlpha ) then
-        fadeInfo.endAlpha = 1.0;
-      end
-      alpha = 0;
-    elseif ( fadeInfo.mode == "OUT" ) then
-      if ( not fadeInfo.startAlpha ) then
-        fadeInfo.startAlpha = 1.0;
-      end
-      if ( not fadeInfo.endAlpha ) then
-        fadeInfo.endAlpha = 0;
-      end
-      alpha = 1.0;
+local frameFadeManager = CreateFrame("FRAME");
+
+-- Generic fade function
+local function UIFrameFade(frame, fadeInfo)
+	if not frame then return end
+	if not fadeInfo.mode then fadeInfo.mode = "IN" end
+	local alpha
+	if fadeInfo.mode == "IN" then
+		if not fadeInfo.startAlpha then fadeInfo.startAlpha = 0 end
+		if not fadeInfo.endAlpha then fadeInfo.endAlpha = 1 end
+		alpha = 0
+    elseif fadeInfo.mode == "OUT" then
+		if not fadeInfo.startAlpha then fadeInfo.startAlpha = 1.0 end
+		if not fadeInfo.endAlpha then fadeInfo.endAlpha = 0 end
+		alpha = 1.0
     end
     frame:SetAlpha(fadeInfo.startAlpha);
+    frame.fadeInfo = fadeInfo
 
-    frame.fadeInfo = fadeInfo;
-
-    local index = 1;
-    while FADEFRAMES[index] do
-      -- If frame is already set to fade then return
-      if ( FADEFRAMES[index] == frame ) then
-        return;
-      end
-      index = index + 1;
+	local index = 1
+	while FADEFRAMES[index] do
+		if ( FADEFRAMES[index] == frame ) then return end -- If frame is already set to fade then return
+		index = index + 1
     end
-    tinsert(FADEFRAMES, frame);
+    tinsert(FADEFRAMES, frame)
     frameFadeManager:SetScript("OnUpdate", UIFrameFade_OnUpdate);
-  end
+end
 
-  -- Convenience function to do a simple fade in
-  local function UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
-    local fadeInfo = {};
-    fadeInfo.mode = "IN";
-    fadeInfo.timeToFade = timeToFade;
-    fadeInfo.startAlpha = startAlpha;
-    fadeInfo.endAlpha = endAlpha;
-    UIFrameFade(frame, fadeInfo);
-  end
+-- Convenience function to do a simple fade in
+local function UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
+	local fadeInfo = {};
+	fadeInfo.mode = "IN";
+	fadeInfo.timeToFade = timeToFade;
+	fadeInfo.startAlpha = startAlpha;
+	fadeInfo.endAlpha = endAlpha;
+	UIFrameFade(frame, fadeInfo);
+end
 
-  -- Convenience function to do a simple fade out
-  local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
-    if IsAddOnLoaded("aCore") and (aCoreCDB == nil or not aCoreCDB.fade) then return end
-    local fadeInfo = {};
-    fadeInfo.mode = "OUT";
-    fadeInfo.timeToFade = timeToFade;
-    fadeInfo.startAlpha = startAlpha;
-    fadeInfo.endAlpha = endAlpha;
-    UIFrameFade(frame, fadeInfo);
-  end
+-- Convenience function to do a simple fade out
+local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
+	if IsAddOnLoaded("aCore") and (aCoreCDB == nil or not aCoreCDB.fade) then return end
+	local fadeInfo = {};
+	fadeInfo.mode = "OUT";
+	fadeInfo.timeToFade = timeToFade;
+	fadeInfo.startAlpha = startAlpha;
+	fadeInfo.endAlpha = endAlpha;
+	UIFrameFade(frame, fadeInfo);
+end
 
 local function Update(self)
 	local unit = self.unit
