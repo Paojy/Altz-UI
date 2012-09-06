c3960f4d8b895492cb6b7a12ad3721fdafbb86d4
Paojy <359661355@qq.com>
2012-09-06 15:48:43 +0800

    code clean global function name change

--- Interface/AddOns/aCore/lib/fader.lua
+++ Interface/AddOns/aCore/lib/fader.lua
@@ -1,151 +1,118 @@
+-- by zork
 
-  -- // fader functionality - frame fading
-  -- // zork - 2012
+local defaultFadeIn   = {time = 0.4, alpha = 1}
+local defaultFadeOut  = {time = 0.4, alpha = 0}
+local defaultEventFadeOut  = {time = 1.5, alpha = 0}
 
-  -----------------------------
-  -- GLOBAL FUNCTIONS
-  -----------------------------
-  --add some global functions
-  local defaultFadeIn   = {time = 0.4, alpha = 1}
-  local defaultFadeOut  = {time = 0.4, alpha = 0}
-  local defaultEventFadeOut  = {time = 1.5, alpha = 0}
-  
-  local frameFadeManager = CreateFrame("FRAME");
+local frameFadeManager = CreateFrame("FRAME");
 
-  -- Generic fade function
-  local function UIFrameFade(frame, fadeInfo)
-    if (not frame) then
-      return;
-    end
-    if ( not fadeInfo.mode ) then
-      fadeInfo.mode = "IN";
-    end
-    local alpha;
-    if ( fadeInfo.mode == "IN" ) then
-      if ( not fadeInfo.startAlpha ) then
-        fadeInfo.startAlpha = 0;
-      end
-      if ( not fadeInfo.endAlpha ) then
-        fadeInfo.endAlpha = 1.0;
-      end
-      alpha = 0;
-    elseif ( fadeInfo.mode == "OUT" ) then
-      if ( not fadeInfo.startAlpha ) then
-        fadeInfo.startAlpha = 1.0;
-      end
-      if ( not fadeInfo.endAlpha ) then
-        fadeInfo.endAlpha = 0;
-      end
-      alpha = 1.0;
+-- Generic fade function
+local function UIFrameFade(frame, fadeInfo)
+	if not frame then return end
+	if not fadeInfo.mode then fadeInfo.mode = "IN" end
+	local alpha
+	if fadeInfo.mode == "IN" then
+		if not fadeInfo.startAlpha then fadeInfo.startAlpha = 0 end
+		if not fadeInfo.endAlpha then fadeInfo.endAlpha = 1 end
+		alpha = 0
+    elseif fadeInfo.mode == "OUT" then
+		if not fadeInfo.startAlpha then fadeInfo.startAlpha = 1.0 end
+		if not fadeInfo.endAlpha then fadeInfo.endAlpha = 0 end
+		alpha = 1.0
     end
     frame:SetAlpha(fadeInfo.startAlpha);
+    frame.fadeInfo = fadeInfo
 
-    frame.fadeInfo = fadeInfo;
-
-    local index = 1;
-    while FADEFRAMES[index] do
-      -- If frame is already set to fade then return
-      if ( FADEFRAMES[index] == frame ) then
-        return;
-      end
-      index = index + 1;
+	local index = 1
+	while FADEFRAMES[index] do
+		if ( FADEFRAMES[index] == frame ) then return end -- If frame is already set to fade then return
+		index = index + 1
     end
-    tinsert(FADEFRAMES, frame);
+    tinsert(FADEFRAMES, frame)
     frameFadeManager:SetScript("OnUpdate", UIFrameFade_OnUpdate);
-  end
+end
 
-  -- Convenience function to do a simple fade in
-  local function UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
-    local fadeInfo = {};
-    fadeInfo.mode = "IN";
-    fadeInfo.timeToFade = timeToFade;
-    fadeInfo.startAlpha = startAlpha;
-    fadeInfo.endAlpha = endAlpha;
-    UIFrameFade(frame, fadeInfo);
-  end
+-- Convenience function to do a simple fade in
+local function UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
+	local fadeInfo = {};
+	fadeInfo.mode = "IN";
+	fadeInfo.timeToFade = timeToFade;
+	fadeInfo.startAlpha = startAlpha;
+	fadeInfo.endAlpha = endAlpha;
+	UIFrameFade(frame, fadeInfo);
+end
 
-  -- Convenience function to do a simple fade out
-  local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
-  	if aCoreCDB == nil or not aCoreCDB.fade then return end
-    local fadeInfo = {};
-    fadeInfo.mode = "OUT";
-    fadeInfo.timeToFade = timeToFade;
-    fadeInfo.startAlpha = startAlpha;
-    fadeInfo.endAlpha = endAlpha;
-    UIFrameFade(frame, fadeInfo);
-  end
+-- Convenience function to do a simple fade out
+local function UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
+	if aCoreCDB == nil or not aCoreCDB.fade then return end
+	local fadeInfo = {};
+	fadeInfo.mode = "OUT";
+	fadeInfo.timeToFade = timeToFade;
+	fadeInfo.startAlpha = startAlpha;
+	fadeInfo.endAlpha = endAlpha;
+	UIFrameFade(frame, fadeInfo);
+end
 
------------------------
--- fade-in on enter/fade-out on leave
------------------------
+--==================================================================--
+--              fade-in on enter/fade-out on leave                  --
+--==================================================================--
 
-  --rButtonBarFader func
-  function ActionbarFader(frame,buttonList,fadeIn,fadeOut)
-    if not frame or not buttonList then return end
-    if not fadeIn then fadeIn = defaultFadeIn end
-    if not fadeOut then fadeOut = defaultFadeOut end
-    frame:EnableMouse(true)
-	
-	frame:SetScript("OnEnter", function(self) if frame.eventmode ~= 1 then UIFrameFadeIn(frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
-    frame:SetScript("OnLeave", function(self) if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
-	
-	for _, button in pairs(buttonList) do
-      if button then
-        button:HookScript("OnEnter", function() if frame.eventmode ~= 1 then UIFrameFadeIn( frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
-        button:HookScript("OnLeave", function() if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
-      end
-    end
-
-	UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha)
-  end
-  
-    --rFrameFader func
-  function rFrameFader(frame,fadeIn,fadeOut)
-    --if 1 == 1 then return end
-    if not frame then return end
-    if not fadeIn then fadeIn = defaultFadeIn end
-    if not fadeOut then fadeOut = defaultFadeOut end
-    frame:EnableMouse(true)
-	
-	-- dont fade by mouse conditions are met
-	frame:SetScript("OnEnter", function(self) if frame.eventmode == 0 then UIFrameFadeIn(frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
-    frame:SetScript("OnLeave", function(self) if frame.eventmode == 0 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
-	
-    UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha)
-  end
-
-	  --rSpellFlyoutFader func
-	  --the flyout is special, when hovering the flyout the parented bar must not fade out
-  function rSpellFlyoutFader(frame,buttonList,fadeIn,fadeOut)
-    if not frame or not buttonList then return end
+function ActionbarFader(frame, buttonList, fadeIn, fadeOut)
+	if not frame or not buttonList then return end
 	if not fadeIn then fadeIn = defaultFadeIn end
 	if not fadeOut then fadeOut = defaultFadeOut end
+	frame:EnableMouse(true)
+
+	frame:SetScript("OnEnter", function(self) if frame.eventmode ~= 1 then UIFrameFadeIn(frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
+	frame:SetScript("OnLeave", function(self) if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
+
+	for _, button in pairs(buttonList) do
+		if button then
+			button:HookScript("OnEnter", function() if frame.eventmode ~= 1 then UIFrameFadeIn( frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
+			button:HookScript("OnLeave", function() if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
+		end
+	end
 	
+	UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha)
+end
+
+function FrameFader(frame,fadeIn,fadeOut)
+	if not frame then return end
+	if not fadeIn then fadeIn = defaultFadeIn end
+	if not fadeOut then fadeOut = defaultFadeOut end
+	frame:EnableMouse(true)
+
+	frame:SetScript("OnEnter", function(self) if frame.eventmode ~= 1 then UIFrameFadeIn(frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
+	frame:SetScript("OnLeave", function(self) if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
+
+	UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha)
+end
+
+--the flyout is special, when hovering the flyout the parented bar must not fade out
+function SpellFlyoutFader(frame,buttonList,fadeIn,fadeOut)
+	if not frame or not buttonList then return end
+	if not fadeIn then fadeIn = defaultFadeIn end
+	if not fadeOut then fadeOut = defaultFadeOut end
+
 	SpellFlyout:SetScript("OnEnter", function() if frame.eventmode ~= 1 then UIFrameFadeIn( frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
 	SpellFlyout:SetScript("OnLeave", function() if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
-	
+
 	for _, button in pairs(buttonList) do
-	  if button then
+		if button then
 		button:HookScript("OnEnter", function() if frame.eventmode ~= 1 then UIFrameFadeIn( frame, fadeIn.time, frame:GetAlpha(), fadeIn.alpha) end end)
 	    button:HookScript("OnLeave", function() if frame.eventmode ~= 1 then UIFrameFadeOut(frame, fadeOut.time, frame:GetAlpha(), fadeOut.alpha) end end)
-	  end
-    end
-  end
------------------------
--- fade-in when center conditions meets
------------------------
---[[
-powerType - A number identifying the power type (number)
-0 - Mana
-1 - Rage
-2 - Focus
-3 - Energy
-6 - Runic Power
-]]
+		end
+	end
+end
+--==================================================================--
+--              fade-in when center conditions meets                --
+--==================================================================--
+--[[   powerType - A number identifying the power type (number)   ]]--
+--[[   0 - Mana 1 - Rage 2 - Focus 3 - Energy 6 - Runic Power     ]]--
 
 function regi(frame)
 	frame:RegisterEvent('PLAYER_REGEN_DISABLED')
-	frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+	frame:RegisterEvent('PLAYER_REGEN_ENABLED')
 	frame:RegisterEvent('UNIT_TARGET')
 	frame:RegisterEvent('PLAYER_TARGET_CHANGED')
 	frame:RegisterEvent('UNIT_HEALTH')
@@ -166,8 +133,6 @@ function ActionbarEventFader(frame,buttonList,fadeIn,fadeOut)
 	if not frame or not buttonList then return end
     if not fadeIn then fadeIn = defaultFadeIn end
     if not fadeOut then fadeOut = defaultEventFadeOut end
-	
-	local powerType = UnitPowerType("player")
 	
 	regi(frame)
 	
@@ -195,8 +160,6 @@ function FrameEventFader(frame,fadeIn,fadeOut)
 	if not frame then return end
     if not fadeIn then fadeIn = defaultFadeIn end
     if not fadeOut then fadeOut = defaultEventFadeOut end
-	
-	local powerType = UnitPowerType("player")
 	
 	regi(frame)
 	
