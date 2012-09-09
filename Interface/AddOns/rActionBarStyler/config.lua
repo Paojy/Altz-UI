d4e0541c691373f5237b11d2ae94aefd9b2702a2
Paojy <359661355@qq.com>
2012-09-09 17:20:38 +0800

    combine bar1 and bar2, bar4 and bar5 new action bar layout

--- Interface/AddOns/rActionBarStyler/config.lua
+++ Interface/AddOns/rActionBarStyler/config.lua
@@ -17,52 +17,10 @@
   --use "/rabs" to see the command list
 
   cfg.bars = {
-    bar1 = {
+    bar12 = {
       enable          = true,
       scale           = 1,
-      padding         = 2, --frame padding
-      buttons         = {
-        size            = 25,
-        margin          = 4,
-      },
-      pos             = { a1 = "TOP", a2 = "CENTER", af = "UIParent", x = 0, y = -145 },
-      userplaced      = {
-        enable          = true,
-      },
-      mouseover       = {
-        enable          = true,
-        fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 0.4, alpha = 0},
-      },
-	  eventfader      = {
-        enable          = true,
-        fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 1.5, alpha = 0},
-      },
-    },
-    overridebar = { --the new vehicle and override bar
-      enable          = true,
-      scale           = 1,
-      padding         = 2, --frame padding
-      buttons         = {
-        size            = 25,
-        margin          = 4,
-      },
-      pos             = { a1 = "TOP", a2 = "CENTER", af = "UIParent", x = 58, y = -145 },
-      userplaced      = {
-        enable          = true,
-      },
-      mouseover       = {
-        enable          = false,
-        fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 0.4, alpha = 0},
-      },
-    },
-    bar2 = {
-      enable          = true,
-	  combineBar23    = true,--by choosing true both bar 2 and 3 will react to the same hover effect, thus show/hide at the same time, settings for bar5 will be ignored
-      scale           = 1,
-      padding         = 2, --frame padding
+      padding         = 4, --frame padding
       buttons         = {
         size            = 25,
         margin          = 4,
@@ -74,30 +32,50 @@
       mouseover       = {
         enable          = true,
         fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 0.4, alpha = 0.2},
-      },		
+        fadeOut         = {time = 0.4, alpha = 0.4},
+      },
 	  eventfader      = {
+        enable          = true,
+        fadeIn          = {time = 0.4, alpha = 1},
+        fadeOut         = {time = 1.5, alpha = 0.4},
+      },
+    },
+    overridebar = { --the new vehicle and override bar
+      enable          = true,
+      scale           = 1,
+      padding         = 4, --frame padding
+      buttons         = {
+        size            = 35,
+        margin          = 4,
+      },
+      pos             = { a1 = "BOTTOMLEFT", a2 = "BOTTOM", af = "UIParent", x = -140, y = 30 },
+      userplaced      = {
+        enable          = false,
+      },
+      mouseover       = {
         enable          = false,
         fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 1.5, alpha = 0.2},
+        fadeOut         = {time = 0.4, alpha = 0},
       },
     },
     bar3 = {
       enable          = true,
+	  layout3x2x2     = true,
+	  space1          = 5, -- the space between the left part and the right part are (bar12's width + 2*space1). only available when enable layout3x2x2. 
       scale           = 1,
-      padding         = 2, --frame padding
+      padding         = 4, --frame padding
       buttons         = {
         size            = 25,
         margin          = 4,
       },
-      pos             = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 0, y = 83 },
+      pos             = { a1 = "BOTTOM", a2 = "BOTTOM", af = "UIParent", x = 0, y = 15 },
       userplaced      = {
         enable          = true,
       },
       mouseover       = {
         enable          = true,
         fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 0.4, alpha = 0},
+        fadeOut         = {time = 0.4, alpha = 0.4},
       },		
 	  eventfader      = {
         enable          = false,
@@ -105,11 +83,10 @@
         fadeOut         = {time = 1.5, alpha = 0},
       },
     },
-    bar4 = {
+    bar45 = {
       enable          = true,
-      combineBar4AndBar5  = true, --by choosing true both bar 4 and 5 will react to the same hover effect, thus show/hide at the same time, settings for bar5 will be ignored
       scale           = 1,
-      padding         = 10, --frame padding
+      padding         = 4, --frame padding
       buttons         = {
         size            = 25,
         margin          = 4,
@@ -129,35 +106,12 @@
         fadeOut         = {time = 0.4, alpha = 0},
       },
     },
-    bar5 = {
-      enable          = true,
-      scale           = 1,
-      padding         = 10, --frame padding
-      buttons         = {
-        size            = 25,
-        margin          = 4,
-      },
-      pos             = { a1 = "RIGHT", a2 = "RIGHT", af = "UIParent", x = -31, y = 0 },
-      userplaced      = {
-        enable          = true,
-      },
-      mouseover       = {
-        enable          = true,
-        fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 0.3, alpha = 0.1},
-      },
-	  eventfader      = {
-        enable          = false,
-        fadeIn          = {time = 0.4, alpha = 1},
-        fadeOut         = {time = 1.5, alpha = 0},
-      },
-    },
     petbar = {
       enable          = true,
 	  show            = true, --true/false
 	  uselayout5x2    = false,
       scale           = 0.7,
-      padding         = 2, --frame padding
+      padding         = 4, --frame padding
       buttons         = {
         size            = 33,
         margin          = 4,
@@ -181,7 +135,7 @@
       enable          = true,
       show            = true, --true/false
       scale           = 1,
-      padding         = 2, --frame padding
+      padding         = 4, --frame padding
       buttons         = {
         size            = 20,
         margin          = 4,
@@ -227,7 +181,7 @@
         size            = 30,
         margin          = 5,
       },
-      pos             = { a1 = "BOTTOMLEFT", a2 = "BOTTOM", af = "UIParent", x = 180, y = 25 },
+      pos             = { a1 = "BOTTOMLEFT", a2 = "BOTTOM", af = "UIParent", x = 280, y = 20 },
       userplaced      = {
         enable          = true,
       },
