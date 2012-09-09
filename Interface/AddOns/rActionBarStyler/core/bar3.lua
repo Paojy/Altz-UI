0b2e64f28e4cb06390132a35f1dce1214445abf1
Paojy <359661355@qq.com>
2012-09-09 17:19:18 +0800

    two style for bar3. 12*1 and 3*2*2

--- Interface/AddOns/rActionBarStyler/core/bar3.lua
+++ Interface/AddOns/rActionBarStyler/core/bar3.lua
@@ -15,15 +15,19 @@
   -----------------------------
 
   if not cfg.enable then return end
-  if gcfg.bars.bar2.combineBar23 then return end
   
   local num = NUM_ACTIONBAR_BUTTONS
   local buttonList = {}
 
   --create the frame to hold the buttons
   local frame = CreateFrame("Frame", "rABS_MultiBarBottomRight", UIParent, "SecureHandlerStateTemplate")
-  frame:SetWidth(num*cfg.buttons.size + (num-1)*cfg.buttons.margin + 2*cfg.padding)
-  frame:SetHeight(cfg.buttons.size + 2*cfg.padding)
+  if cfg.layout3x2x2 then
+    frame:SetWidth(num/2*cfg.buttons.size +(num/2-2)*cfg.buttons.margin +2*cfg.padding +num*gcfg.bars.bar12.buttons.size +(num-1)*gcfg.bars.bar12.buttons.margin +2*cfg.padding +2*cfg.space1)
+    frame:SetHeight(2*cfg.buttons.size + 2*cfg.padding + cfg.buttons.margin)
+  else
+    frame:SetWidth(num*cfg.buttons.size + (num-1)*cfg.buttons.margin + 2*cfg.padding)
+    frame:SetHeight(cfg.buttons.size + 2*cfg.padding)
+  end
   frame:SetPoint(cfg.pos.a1,cfg.pos.af,cfg.pos.a2,cfg.pos.x,cfg.pos.y)
   frame:SetScale(cfg.scale)
 
@@ -37,8 +41,14 @@
     button:SetSize(cfg.buttons.size, cfg.buttons.size)
     button:ClearAllPoints()
     if i == 1 then
-      button:SetPoint("LEFT", frame, cfg.padding, 0)
-    else
+      button:SetPoint("TOPLEFT", frame, cfg.padding, -cfg.padding)
+	elseif cfg.layout3x2x2 and i == 4 then
+	  button:SetPoint("TOP", "MultiBarBottomRightButton1", "BOTTOM", 0, -cfg.buttons.margin)
+	elseif cfg.layout3x2x2 and i == 7 then
+	  button:SetPoint("TOPRIGHT", frame, -cfg.padding -2*cfg.buttons.size -2*cfg.buttons.margin, -cfg.padding)
+	elseif cfg.layout3x2x2 and i == 10 then
+	  button:SetPoint("TOP", "MultiBarBottomRightButton7", "BOTTOM", 0, -cfg.buttons.margin)
+	else
       local previous = _G["MultiBarBottomRightButton"..i-1]
       button:SetPoint("LEFT", previous, "RIGHT", cfg.buttons.margin, 0)
     end
