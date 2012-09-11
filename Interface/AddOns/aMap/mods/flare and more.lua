a9c0f4ed6cb12a7a27e76e5df1c7f51c75ae3223
Paojy <359661355@qq.com>
2012-09-11 22:29:03 +0800

    bug fix

--- Interface/AddOns/aMap/mods/flare and more.lua
+++ Interface/AddOns/aMap/mods/flare and more.lua
@@ -1,44 +1,29 @@
 ﻿--[[---------
 ## Title: Flare
 ## Author: 楼上你妈妈叫你吃饭，Athelas
-
 --]]--------
+local Ccolor = GetClassColor()
 local wm = CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButton
 wm:SetParent("UIParent")
 wm:ClearAllPoints()
 wm:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", -3, -3) 
-wm:SetSize(16, 16)
+wm:SetSize(20, 20)
+creategrowBD(wm, Ccolor.r, Ccolor.g, Ccolor.b, 0.3, 1)
+wm.border:SetAllPoints(wm)
 wm:Hide()
 
 CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButtonLeft:SetAlpha(0) 
 CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButtonMiddle:SetAlpha(0) 
 CompactRaidFrameManagerDisplayFrameLeaderOptionsRaidWorldMarkerButtonRight:SetAlpha(0) 
-wm:SetHighlightTexture("")
 wm:SetDisabledTexture("")
+wm:SetHighlightTexture("")
 
-local wmmenuFrame = CreateFrame("Frame", "wmRightClickMenu", UIParent, "UIDropDownMenuTemplate")
-local wmmenuList = { 
-	{text = "Ready Check", func = function() DoReadyCheck() end}, 
-	{text = "Initiate Role Poll", func = function() InitiateRolePoll() end}, 
-	{text = "Convert To Raid", func = function() ConvertToRaid() end}, 
-	{text = "Convert To Party", func = function() ConvertToParty() end}, 
-} 
+wm:RegisterEvent("GROUP_ROSTER_UPDATE")
 
-wm:SetScript('OnMouseUp', function(self, button) 
-	if (button=="RightButton") then 
-		EasyMenu(wmmenuList, wmmenuFrame, "cursor", -150, 0, "MENU", 2) 
-	end 
-end)
-
-wm:RegisterEvent("PARTY_MEMBERS_CHANGED")
-wm:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-wm:HookScript("OnEvent", function(self) 
-	local inInstance, instanceType = IsInInstance()
-	if (UnitIsRaidOfficer("player") or UnitIsGroupLeader("player")) and not (inInstance and (instanceType == "raid"))
-	or (inInstance and (instanceType == "party")) then
+wm:HookScript("OnEvent", function(self, event) 
+	if UnitIsGroupAssistant("player") or UnitIsGroupLeader("player") or (IsInGroup() and not IsInRaid()) then
 		self:Show()
 	else
 		self:Hide()
 	end
-end) 
+end)
