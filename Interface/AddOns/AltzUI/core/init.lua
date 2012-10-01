8a8c40b652e5abd01ce6b4f493ac17cab1b95a71
Paojy <359661355@qq.com>
2012-10-01 14:24:20 +0800

    add some global values

--- Interface/AddOns/AltzUI/core/init.lua
+++ Interface/AddOns/AltzUI/core/init.lua
@@ -7,15 +7,31 @@ ns[2] = {} -- C, config
 ns[3] = {} -- L, localization
 ns[4] = {} -- G, globals (Optionnal)
 
+--[[--------------
+--     init     --
+--------------]]--
+
 local T, C, L, G = unpack(select(2, ...))
 
 G.dragFrameList = {}
 
+G.norFont = GameFontHighlight:GetFont()
+G.numFont = "Interface\\AddOns\\AltzUI\\media\\number.ttf"
+
+G.Client = GetLocale()
+G.Version = GetAddOnMetadata("AltzUIConfig", "Version")
+
+G.resolution = GetCVar("gxResolution")
+G.screenheight = tonumber(string.match(G.resolution, "%d+x(%d+)"))
+G.screenwidth = tonumber(string.match(G.resolution, "(%d+)x+%d"))
+
+G.myClass = select(2, UnitClass("player"))
+
 G.Ccolor = {}
 if(IsAddOnLoaded'!ClassColors' and CUSTOM_CLASS_COLORS) then
-	G.Ccolor = CUSTOM_CLASS_COLORS[select(2, UnitClass("player"))]
+	G.Ccolor = CUSTOM_CLASS_COLORS[G.myClass]
 else
-	G.Ccolor = RAID_CLASS_COLORS[select(2, UnitClass("player"))]
+	G.Ccolor = RAID_CLASS_COLORS[G.myClass]
 end
 
 G.Ccolors = {}
@@ -24,9 +40,3 @@ if(IsAddOnLoaded'!ClassColors' and CUSTOM_CLASS_COLORS) then
 else
 	G.Ccolors = RAID_CLASS_COLORS
 end
-
-G.Version = GetAddOnMetadata("AltzUIConfig", "Version")
-G.Client = GetLocale()
-G.resolution = GetCVar("gxResolution")
-G.screenheight = tonumber(string.match(G.resolution, "%d+x(%d+)"))
-G.screenwidth = tonumber(string.match(G.resolution, "(%d+)x+%d"))
