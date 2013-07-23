0d0e1da3047c8b18069a2de0d050a2be394f7207
Paojy <359661355@qq.com>
2013-07-23 22:52:21 +0800

    分离弹出的对话窗口

---
+++ AddOns/AltzUIConfig/PopupDialogs.lua
@@ -0,0 +1,41 @@
+local T, C, L, G = unpack(select(2, ...))
+
+StaticPopupDialogs[G.uiname.."incorrect item ID"] = {
+	text = L["Incorrect Item ID"],
+	button1 = ACCEPT, 
+	hideOnEscape = 1, 
+	whileDead = true,
+	preferredIndex = 3,
+}
+
+StaticPopupDialogs[G.uiname.."incorrect item quantity"]= {
+	text = L["Incorrect Quantity"],
+	button1 = ACCEPT, 
+	hideOnEscape = 1, 
+	whileDead = true,
+	preferredIndex = 3,
+}
+
+StaticPopupDialogs[G.uiname.."incorrect spellid"] = {
+	text = L["Incorret Spell"],
+	button1 = ACCEPT, 
+	hideOnEscape = 1, 
+	whileDead = true,
+	preferredIndex = 3,
+}
+
+StaticPopupDialogs[G.uiname.."incorrect spell"] = {
+	text = L["incorrect spell name"],
+	button1 = ACCEPT, 
+	hideOnEscape = 1, 
+	whileDead = true,
+	preferredIndex = 3,
+}
+
+StaticPopupDialogs[G.uiname.."incorrect level"] = {
+	text = L["should be a number."],
+	button1 = ACCEPT, 
+	hideOnEscape = 1, 
+	whileDead = true,
+	preferredIndex = 3,
+}
