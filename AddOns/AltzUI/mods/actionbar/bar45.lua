37db4f28f1cb9172fec0c4a6985f7d4a01ebf965
Paojy <359661355@qq.com>
2015-07-06 11:54:22 +0800

    增加横向排列右侧动作条的功能

--- AddOns/AltzUI/mods/actionbar/bar45.lua
+++ AddOns/AltzUI/mods/actionbar/bar45.lua
@@ -14,6 +14,8 @@ local eventfader= {
 	fadeIn= {time = 0.4, alpha = 1},
 	fadeOut = {time = 1.5, alpha = aCoreCDB["ActionbarOptions"]["bar45fademinaplha"]},
 	}
+	
+local Horizontalbar45 = aCoreCDB["ActionbarOptions"]["Horizontalbar45"]
 
 -- FUNCTIONS
 
@@ -24,12 +26,10 @@ local buttonList = {}
 local frame = CreateFrame("Frame", "Altz_Bar4&5", UIParent, "SecureHandlerStateTemplate")
 frame.movingname = L["右侧额外动作条"]
 frame.point = {
-	healer = {a1 = "BOTTOMRIGHT", parent = "UIParent", a2 = "BOTTOMRIGHT", x = -20, y = 195},
-	dpser = {a1 = "BOTTOMRIGHT", parent = "UIParent", a2 = "BOTTOMRIGHT", x = -20, y = 195},
+	healer = {a1 = "BOTTOMRIGHT", parent = "UIParent", a2 = "BOTTOMRIGHT", x = -20, y = 230},
+	dpser = {a1 = "BOTTOMRIGHT", parent = "UIParent", a2 = "BOTTOMRIGHT", x = -20, y = 230},
 }
 T.CreateDragFrame(frame)
-frame:SetWidth(2*buttonssize + (2-1)*buttonspace + 2*padding)
-frame:SetHeight(num*buttonssize + (num-1)*buttonspace + 2*padding)
 
 --move the buttons into position and reparent them
 MultiBarRight:SetParent(frame)
@@ -37,32 +37,65 @@ MultiBarRight:EnableMouse(false)
 MultiBarLeft:SetParent(frame)
 MultiBarLeft:EnableMouse(false)
 
-for i=1, num do
-	local button = _G["MultiBarRightButton"..i]
-	table.insert(buttonList, button) --add the button object to the list
-	button:SetSize(buttonssize, buttonssize)
-	button:ClearAllPoints()
-	if i == 1 then
-		button:SetPoint("TOPRIGHT", frame, -padding, -padding)
-	else
-		local previous = _G["MultiBarRightButton"..i-1]
-		button:SetPoint("TOP", previous, "BOTTOM", 0, -buttonspace)
+if Horizontalbar45 then
+	frame:SetHeight(2*buttonssize + (2-1)*buttonspace + 2*padding)
+	frame:SetWidth(num*buttonssize + (num-1)*buttonspace + 2*padding)
+
+	for i=1, num do
+		local button = _G["MultiBarRightButton"..i]
+		table.insert(buttonList, button) --add the button object to the list
+		button:SetSize(buttonssize, buttonssize)
+		button:ClearAllPoints()
+		if i == 1 then
+			button:SetPoint("TOPLEFT", frame, padding, -padding)
+		else
+			local previous = _G["MultiBarRightButton"..i-1]
+			button:SetPoint("LEFT", previous, "RIGHT", buttonspace, 0)
+		end
+	end
+
+	for i=1, num do
+		local button = _G["MultiBarLeftButton"..i]
+		table.insert(buttonList, button) --add the button object to the list
+		button:SetSize(buttonssize, buttonssize)
+		button:ClearAllPoints()
+		if i == 1 then
+			button:SetPoint("TOPLEFT", frame, padding, -(padding+buttonspace+buttonssize))
+		else
+			local previous = _G["MultiBarLeftButton"..i-1]
+			button:SetPoint("LEFT", previous, "RIGHT", buttonspace, 0)
+		end
+	end
+else
+	frame:SetWidth(2*buttonssize + (2-1)*buttonspace + 2*padding)
+	frame:SetHeight(num*buttonssize + (num-1)*buttonspace + 2*padding)
+
+	for i=1, num do
+		local button = _G["MultiBarRightButton"..i]
+		table.insert(buttonList, button) --add the button object to the list
+		button:SetSize(buttonssize, buttonssize)
+		button:ClearAllPoints()
+		if i == 1 then
+			button:SetPoint("TOPRIGHT", frame, -padding, -padding)
+		else
+			local previous = _G["MultiBarRightButton"..i-1]
+			button:SetPoint("TOP", previous, "BOTTOM", 0, -buttonspace)
+		end
+	end
+
+	for i=1, num do
+		local button = _G["MultiBarLeftButton"..i]
+		table.insert(buttonList, button) --add the button object to the list
+		button:SetSize(buttonssize, buttonssize)
+		button:ClearAllPoints()
+		if i == 1 then
+			button:SetPoint("TOPRIGHT", frame, -(padding+buttonspace+buttonssize), -padding)
+		else
+			local previous = _G["MultiBarLeftButton"..i-1]
+			button:SetPoint("TOP", previous, "BOTTOM", 0, -buttonspace)
+		end
 	end
 end
-
-for i=1, num do
-	local button = _G["MultiBarLeftButton"..i]
-	table.insert(buttonList, button) --add the button object to the list
-	button:SetSize(buttonssize, buttonssize)
-	button:ClearAllPoints()
-	if i == 1 then
-		button:SetPoint("TOPRIGHT", frame, -(padding+buttonspace+buttonssize), -padding)
-	else
-		local previous = _G["MultiBarLeftButton"..i-1]
-		button:SetPoint("TOP", previous, "BOTTOM", 0, -buttonspace)
-	end
-end
-
 --hide the frame when in a vehicle!
 RegisterStateDriver(frame, "visibility", "[petbattle][overridebar][vehicleui][possessbar,@vehicle,exists] hide; show")
 
