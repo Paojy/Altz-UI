72adf6bc6dcd743a882a0c598bcc6f9727c0fa92
Paojy <hongshaoww@163.com>
2016-08-19 01:23:31 +0800

    修复skada皮肤

--- Interface/AddOns/AltzUI/mods/skins/Skada.lua
+++ Interface/AddOns/AltzUI/mods/skins/Skada.lua
@@ -8,22 +8,20 @@ local blank = "Interface\\AddOns\\AltzUI\\media\\statusbar"
 
 barmod.ApplySettings_ = barmod.ApplySettings
 barmod.ApplySettings = function(self, win)
-barmod.ApplySettings_(self, win)
+	barmod.ApplySettings_(self, win)
 	
 	local skada = win.bargroup
 
 	skada:SetTexture(blank)
 	skada:SetSpacing(1, 1)
-	skada:SetFont(font, 10)
-	skada:SetFrameLevel(5)
+	skada:SetFont(G.norFont, 12)
 	
 	skada:SetBackdrop(nil)
+	skada.borderFrame:SetBackdrop(nil)
+	
 	if not skada.border then
-		skada.border = F.CreateBDFrame(skada, 0.3)
-		skada.border:ClearAllPoints()
-		skada.border:SetPoint('TOPLEFT', win.bargroup.button or win.bargroup, 'TOPLEFT')
-		skada.border:SetPoint('BOTTOMRIGHT', win.bargroup, 'BOTTOMRIGHT')
-		skada.border:SetBackdrop(nil)
+		skada.border = CreateFrame("Frame", G.uiname..skada:GetName().."Skin", skada)
+		skada.border:SetAllPoints(skada.borderFrame)
 		F.SetBD(skada.border)
 	end
 end
