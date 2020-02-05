28d55dcfcf6585949fc6749b944068651c17299a
Paojy <359661355@qq.com>
2020-02-05 14:56:57 +0800

    增加boss头像框体默认间距

--- Interface/AddOns/AltzUI/mods/unitframes/core.lua
+++ Interface/AddOns/AltzUI/mods/unitframes/core.lua
@@ -1532,7 +1532,7 @@ function EventFrame:ADDON_LOADED(arg1)
 					}
 				else
 					bossframes["boss"..i].point = {
-						healer = {a1 = "TOP", parent = bossframes["boss"..(i-1)]:GetName(), a2 = "BOTTOM" , x = 0, y = -60},
+						healer = {a1 = "TOP", parent = bossframes["boss"..(i-1)]:GetName(), a2 = "BOTTOM" , x = 0, y = -90},
 						dpser = {a1 = "TOP", parent = bossframes["boss"..(i-1)]:GetName(), a2 = "BOTTOM" , x = 0, y = -60},
 					}
 				end
