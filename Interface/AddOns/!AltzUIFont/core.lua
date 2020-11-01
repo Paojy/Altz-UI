8e10461498672c06da1340442a8c0042d4988012
Paojy <359661355@qq.com>
2020-11-01 14:45:26 +0800

    调整字体调用方式，现在字体在AltzUI/media/font.ttf，Aurora可以单独更新

---
+++ Interface/AddOns/!AltzUIFont/core.lua
@@ -0,0 +1,8 @@
+﻿local f = CreateFrame("Frame", "AltzUIFont", UIParent)
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)	
+	if addon == "AuroraClassic" then
+		local _, C = unpack(AuroraClassic)
+		C.Font[1] =  "Interface\\AddOns\\AltzUI\\media\\font.ttf" -- 字体路径
+	end
+end)
