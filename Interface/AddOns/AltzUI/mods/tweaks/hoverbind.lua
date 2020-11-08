a0fae08497c69ea1debefc3cfe36f7bb3f649e93
Paojy <359661355@qq.com>
2020-11-09 03:27:54 +0800

    快捷绑定按键关闭后不再弹出绑定界面

--- Interface/AddOns/AltzUI/mods/tweaks/hoverbind.lua
+++ Interface/AddOns/AltzUI/mods/tweaks/hoverbind.lua
@@ -2,12 +2,22 @@
 local T, C, L, G = unpack(select(2, ...))
 local F = unpack(AuroraClassic)
 
-local bind, localmacros = CreateFrame("Frame", "ncHoverBind", UIParent), 0
+local hide
 -- SLASH COMMAND
 SlashCmdList.MOUSEOVERBIND = function()
 	LoadAddOn("Blizzard_BindingUI")
 	KeyBindingFrame.keyID = 1; 
 	KeyBindingFrame:EnterQuickKeybind();
+	
+	if not hide then
+		QuickKeybindFrame:HookScript("OnHide", function()
+			HideUIPanel(KeyBindingFrame)
+		end)
+		KeyBindingFrame:HookScript("OnHide", function()
+			HideUIPanel(GameMenuFrame)
+		end)
+		hide = true
+	end
 end
 
 SLASH_MOUSEOVERBIND1 = "/hb"
