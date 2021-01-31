98fe9e48e5797a05ca3861a25b176ffc043cacd7
Paojy <359661355@qq.com>
2021-01-31 16:00:06 +0800

    重写bw皮肤

--- Interface/AddOns/AltzUI/mods/skins/Skin.lua
+++ Interface/AddOns/AltzUI/mods/skins/Skin.lua
@@ -1,128 +1,117 @@
 local NAME, addon = ...
 
-local f = CreateFrame("Frame")
-local function registerBWStyle()
-    if not BigWigs or not aCoreCDB["SkinOptions"]["setBW"] then return end
-    local bars = BigWigs:GetPlugin("Bars", true)
-    if not bars then return end
-    local fontName, fontSize, fontArgs = AltzUISkinFont:GetFont()
+local glowTex = "Interface\\AddOns\\AltzUI\\media\\glow"
+local bgTex = "Interface\\AddOns\\AltzUI\\media\\statusbar"
 
-    -- based on MonoUI style
-    local backdropBorder = {
-        bgFile = [[Interface\AddOns\AltzUI\media\statusbar]],
-        edgeFile = [[Interface\AddOns\AltzUI\media\glow]],
-        tile = false, tileSize = 0, edgeSize = 2,
-        insets = {left = 2, right = 2, top = 2, bottom = 2}
-    }
+local backdropBorder = {
+	bgFile = bgTex,
+	edgeFile = glowTex,
+	tile = false, tileSize = 0, edgeSize = 3,
+	insets = {left = 3, right = 3, top = 3, bottom = 3}
+}
 
-    local function styleBar(bar)
-        --print("styleBar", bar)
-        bar:SetHeight(20)
-        bar.candyBarBackground:Hide()
+local floor = floor
+local fontName, fontSize, fontArgs = AltzUISkinFont:GetFont()
 
-        local bd = bar.candyBarBackdrop
-        bd:SetBackdrop(backdropBorder)
-        bd:SetBackdropColor(0, 0, 0, 0.5)
-        bd:SetBackdropBorderColor(0, 0, 0, 1)
+local function removeStyle(bar)
+	local cbb = bar.candyBarBar
 
-        bd:ClearAllPoints()
-        bd:SetPoint("TOPLEFT", bar, "TOPLEFT", -2, 2)
-        bd:SetPoint("BOTTOMRIGHT", bar, "BOTTOMRIGHT", 2, -2)
-        bd:Show()
+	bar.candyBarBackdrop:Hide()
+	local height = bar:Get("bigwigs:restoreheight")
+	if height then
+		bar:SetHeight(height)
+	end
 
-        if bars.db.profile.icon then
-            local icon = bar.candyBarIconFrame
-            local tex = icon.icon
-            bar:SetIcon(nil)
-            icon:SetTexture(tex)
-            icon:ClearAllPoints()
-            icon:SetPoint("BOTTOMRIGHT", bar, "BOTTOMLEFT", -4, 0)
-            icon:SetSize(24, 24)
-            icon:Show() -- XXX temp
-            bar:Set("bigwigs:restoreIcon", tex)
+	local tex = bar:Get("bigwigs:restoreicon")
+	if tex then
+		bar:SetIcon(tex)
+		bar:Set("bigwigs:restoreicon", nil)
+		bar.candyBarIconFrameBackdrop:Hide()
+	end
 
-            local iconBd = bar.candyBarIconFrameBackdrop
-            iconBd:SetBackdrop(backdropBorder)
-            iconBd:SetBackdropColor(0, 0, 0, 0.5)
-            iconBd:SetBackdropBorderColor(0, 0, 0, 1)
-
-            iconBd:ClearAllPoints()
-            iconBd:SetPoint("TOPLEFT", icon, "TOPLEFT", -2, 2)
-            iconBd:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 2, -2)
-            iconBd:Show()
-        end
-
-        local label = bar.candyBarLabel
-        local font = label:GetFontObject() or {label:GetFont()}
-        bar:Set("bigwigs:restoreFont", font)
-        local shadow = {label:GetShadowOffset()}
-        bar:Set("bigwigs:restoreShadow", shadow)
-
-        label:SetFont(fontName, fontSize, fontArgs)
-        label:SetShadowOffset(0, 0)
-        label:SetJustifyH("LEFT")
-        label:SetJustifyV("BOTTOM")
-        label:ClearAllPoints()
-        label:SetPoint("LEFT", bar, "LEFT", 4, 0)
-        label:SetPoint("RIGHT", bar, "RIGHT", -25, 0)
-
-        local timer = bar.candyBarDuration
-        timer:SetFont(fontName, fontSize, fontArgs)
-        timer:SetShadowOffset(0, 0)
-        timer:SetJustifyH("RIGHT")
-        timer:ClearAllPoints()
-        timer:SetPoint("RIGHT", bar, "RIGHT", -4, 0)
-
-        bar:SetTexture([[Interface\AddOns\AltzUI\media\statusbar]])
-    end
-
-    local function removeStyle(bar)
-        bar:SetHeight(14)
-        bar.candyBarBackdrop:Hide()
-        bar.candyBarBackground:Show()
-
-        local tex = bar:Get("bigwigs:restoreIcon")
-        if tex then
-            local icon = bar.candyBarIconFrame
-            icon:ClearAllPoints()
-            icon:SetPoint("TOPLEFT")
-            icon:SetPoint("BOTTOMLEFT")
-            bar:SetIcon(tex)
-
-            bar.candyBarIconFrameBackdrop:Hide()
-        end
-
-        --local shadow = bar:Get("bigwigs:restoreShadow")
-        local label = bar.candyBarLabel
-        --label:SetShadowOffset(shadow[1], shadow[2])
-        label:ClearAllPoints()
-        label:SetPoint("TOPLEFT", bar.candyBarBar, "TOPLEFT", 2, 0)
-        label:SetPoint("BOTTOMRIGHT", bar.candyBarBar, "BOTTOMRIGHT", -2, 0)
-
-        local timer = bar.candyBarDuration
-       -- timer:SetShadowOffset(shadow[1], shadow[2])
-        timer:ClearAllPoints()
-        timer:SetPoint("RIGHT", bar.candyBarBar, "RIGHT", -2, 0)
-
-        local font = bar:Get("bigwigs:restoreFont")
-        if type(font) == "table" and font[1] then
-            label:SetFont(font[1], floor(font[2] + 0.5), font[3])
-            timer:SetFont(font[1], floor(font[2] + 0.5), font[3])
-        else
-            label:SetFontObject(font)
-            timer:SetFontObject(font)
-        end
-    end
-
-    bars:RegisterBarStyle("AltzUI", {
-        apiVersion = 1,
-        version = 1,
-        GetSpacing = function(bar) return 8 end,
-        ApplyStyle = styleBar,
-        BarStopped = removeStyle,
-        GetStyleName = function() return "AltzUI" end,
-    })
+	local timer = bar.candyBarDuration
+	timer:ClearAllPoints()
+	timer:SetPoint("TOPLEFT", cbb, "TOPLEFT", 2, 0)
+	timer:SetPoint("BOTTOMRIGHT", cbb, "BOTTOMRIGHT", -2, 0)
+	
+	local label = bar.candyBarLabel
+	label:ClearAllPoints()
+	label:SetPoint("TOPLEFT", cbb, "TOPLEFT", 2, 0)
+	label:SetPoint("BOTTOMRIGHT", cbb, "BOTTOMRIGHT", -2, 0)
 end
+
+local function styleBar(bar)
+	local cbb = bar.candyBarBar
+
+	local height = bar:GetHeight()
+	bar:Set("bigwigs:restoreheight", height)
+	bar:SetHeight(height)
+
+	local bd = bar.candyBarBackdrop
+	bd:SetBackdrop(backdropBorder)
+	bd:SetBackdropColor(0, 0, 0, .4)
+	bd:SetBackdropBorderColor(0, 0, 0, 1)
+	bd:ClearAllPoints()
+	bd:SetPoint("TOPLEFT", bar, "TOPLEFT", -3, 3)
+	bd:SetPoint("BOTTOMRIGHT", bar, "BOTTOMRIGHT", 3, -3)
+	bd:Show()
+
+	local tex = bar:GetIcon()
+	if tex then
+		local icon = bar.candyBarIconFrame
+		bar:SetIcon(nil)
+		icon:SetTexture(tex)
+		icon:Show()
+		if bar.iconPosition == "RIGHT" then
+			icon:SetPoint("BOTTOMLEFT", bar, "BOTTOMRIGHT", 5, 0)
+		else
+			icon:SetPoint("BOTTOMRIGHT", bar, "BOTTOMLEFT", -5, 0)
+		end
+		icon:SetSize(height+2, height+2)
+		bar:Set("bigwigs:restoreicon", tex)
+
+		local iconBd = bar.candyBarIconFrameBackdrop
+		iconBd:SetBackdrop(backdropBorder)
+		iconBd:SetBackdropColor(.15, .15, .15, .4)
+		iconBd:SetBackdropBorderColor(0, 0, 0, 1)
+		iconBd:ClearAllPoints()
+		iconBd:SetPoint("TOPLEFT", icon, "TOPLEFT", -3, 3)
+		iconBd:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 3, -3)
+		iconBd:Show()
+	end
+	
+	local label = bar.candyBarLabel
+	label:SetFont(fontName, fontSize, fontArgs)
+    label:SetShadowOffset(0, 0)
+    label:ClearAllPoints()
+    label:SetPoint("LEFT", bar, "LEFT", 4, 0)
+    label:SetPoint("RIGHT", bar, "RIGHT", -25, 0)
+	
+	local timer = bar.candyBarDuration
+    timer:SetFont(fontName, fontSize, fontArgs)
+    timer:SetShadowOffset(0, 0)
+    timer:SetJustifyH("RIGHT")
+    timer:ClearAllPoints()
+    timer:SetPoint("RIGHT", bar, "RIGHT", -4, 0)
+	
+	bar:SetTexture(bgTex)
+end
+
+BigWigsAPI:RegisterBarStyle("AltzUI", {
+	apiVersion = 1,
+	version = 11,
+	barSpacing = 24,
+	barHeight = 24,	
+	fontSizeNormal = 22,
+	fontSizeEmphasized = 22,
+	fontOutline = "OUTLINE",
+	ApplyStyle = styleBar,
+	BarStopped = removeStyle,
+	GetStyleName = function() return "AltzUI" end,
+})
+
+
+local f = CreateFrame("Frame")
 
 local function registerDBMStyle()
     if not DBM or not aCoreCDB["SkinOptions"]["setDBM"] then return end
