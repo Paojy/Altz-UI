8bb1de31db2e014992c752851e2bb8a817c67198
Paojy <359661355@qq.com>
2022-10-31 18:45:08 +0800

    删除 任务追踪移动功能

--- Interface/AddOns/AltzUI/mods/tweaks/watchframe.lua
+++ Interface/AddOns/AltzUI/mods/tweaks/watchframe.lua
@@ -1,6 +1,5 @@
 --Original Author: Nibelheim
 local T, C, L, G = unpack(select(2, ...))
-local dragFrameList = G.dragFrameList
 
 local eventframe = CreateFrame("Frame")
 eventframe:RegisterEvent("PLAYER_ENTERING_WORLD")
@@ -13,19 +12,6 @@ eventframe:SetScript("OnEvent", function()
 		end
 	end
 end)
-
-local anchorframe = CreateFrame("Frame", "Altz_WFanchorframe", UIParent)
-local customobjectivetracker = aCoreCDB["SkinOptions"]["customobjectivetracker"]
-if customobjectivetracker then return end
- 
-anchorframe.movingname = L["任务追踪"]
-anchorframe.point = {
-	healer = {a1 = "TOPLEFT", parent = "UIParent", a2 = "TOPLEFT", x = 20, y = -200},
-	dpser = {a1 = "TOPLEFT", parent = "UIParent", a2 = "TOPLEFT", x = 20, y = -200},
-}
-T.CreateDragFrame(anchorframe) --frame, dragFrameList, inset, clamp	
-anchorframe:SetSize(240, 20)
-
 
 OBJECTIVE_TRACKER_COLOR["Normal"]["r"] = 1
 OBJECTIVE_TRACKER_COLOR["Normal"]["g"] = 1
@@ -46,14 +32,3 @@ OBJECTIVE_TRACKER_COLOR["HeaderHighlight"]["b"] = G.Ccolor.b
 OBJECTIVE_TRACKER_COLOR["Complete"]["r"] = 1
 OBJECTIVE_TRACKER_COLOR["Complete"]["g"] = 1
 OBJECTIVE_TRACKER_COLOR["Complete"]["b"] = 1
-
-
-local vm = ObjectiveTrackerFrame
-
-vm:SetFrameStrata("MEDIUM")
-vm:SetFrameLevel(15) -- higher than multiright actionbar
-vm:ClearAllPoints()
-vm:SetPoint("TOPLEFT", anchorframe, "TOPLEFT")
-vm:SetHeight(G.screenheight - 300)
-vm.ClearAllPoints = function() end
-vm.SetPoint = function() end
