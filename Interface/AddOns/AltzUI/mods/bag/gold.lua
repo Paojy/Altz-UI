c26ab08ccd691d185b1485903c80b1522e75ccc3
Paojy <359661355@qq.com>
2023-05-26 03:23:54 +0800

    Update gold.lua

--- Interface/AddOns/AltzUI/mods/bag/gold.lua
+++ Interface/AddOns/AltzUI/mods/bag/gold.lua
@@ -1,8 +1,6 @@
 local T, C, L, G = unpack(select(2, ...))
 local F, C = unpack(AuroraClassic)
 
-if not aCoreCDB["ItemOptions"]["enablebag"] then return end
-
 local Profit, Spent, OldMoney = 0, 0, 0
 
 local function formatMoney(money)
