8d7f9304a688055a4fd93ef3f783dec3a49536a4
Paojy <359661355@qq.com>
2023-05-31 18:40:54 +0800

    呼吸条等皮肤更新

--- Interface/AddOns/AltzUI/mods/skins/Mirrortimer.lua
+++ Interface/AddOns/AltzUI/mods/skins/Mirrortimer.lua
@@ -6,33 +6,39 @@ local total = MIRRORTIMER_NUMTIMERS
 local function Skin(timer, value, maxvalue, scale, paused, label)
 	for i = 1, total, 1 do
 		local frame = _G["MirrorTimer"..i]
+		local statusbar = frame.StatusBar
+		local border = frame.Border
+		local text = frame.Text
+		local textborder = frame.TextBorder
+		
 		if not frame.isSkinned then
-			local statusbar = _G[frame:GetName().."StatusBar"]
-			local border = _G[frame:GetName().."Border"]
-			local text = _G[frame:GetName().."Text"]
-			
 			statusbar:ClearAllPoints()
 			statusbar:SetPoint("TOPLEFT", frame, 2, -2)
 			statusbar:SetPoint("BOTTOMRIGHT", frame, -2, 8)
-			statusbar:SetStatusBarTexture("Interface\\AddOns\\AltzUI\\media\\statusbar")
-			
-			local region = frame:GetRegions()
-			if region:GetObjectType() == "Texture" then
-				region:SetAlpha(0)
+
+			local regions = {frame:GetRegions()}
+			for k, region in pairs(regions) do
+				if region:GetObjectType() == "Texture" then
+					region:SetAlpha(0)
+				end
 			end
-		
+			
 			statusbar.backdrop = F.CreateBDFrame(statusbar, 0.5)
-			statusbar.backdrop:SetPoint("BOTTOMRIGHT", statusbar, 1, -2)
-			T.CreateSD(statusbar.backdrop, 2, 0, 0, 0, 0, -1)
+			statusbar.backdrop:SetPoint("BOTTOMRIGHT", statusbar, 1, -1)
+			T.CreateSD(statusbar.backdrop, 2)
 			
 			text:ClearAllPoints()
 			text:SetFont(G.norFont, 12, "OUTLINE")
 			text:SetPoint("CENTER", statusbar)
 			
 			border:SetTexture(nil)
+			textborder:Hide()
 			
 			frame.isSkinned = true
 		end
+		
+		statusbar:SetStatusBarTexture(G.media.ufbar)
+		statusbar:SetStatusBarColor(0, .7, 1)
 	end
 end
 
