908c7454b881bc1f138ad6733aac7dba4c2e2f47
Paojy <359661355@qq.com>
2024-04-12 17:58:10 +0800

    聊天频道缩写即时生效

--- Interface/AddOns/AltzUI/mods/chat/channelreplacements.lua
+++ Interface/AddOns/AltzUI/mods/chat/channelreplacements.lua
@@ -1,41 +1,69 @@
 local T, C, L, G = unpack(select(2, ...))
 
 local EventFrame = CreateFrame('Frame')
-EventFrame:SetScript('OnEvent', function(self, event, arg)
-	if arg ~= "AltzUI" then return end
-	if not aCoreCDB["ChatOptions"]["channelreplacement"] then return end
+
+local Defaults = {}
+local EditData = {
 	--guild
-	CHAT_GUILD_GET = "|Hchannel:GUILD|hG|h %s "
-	CHAT_OFFICER_GET = "|Hchannel:OFFICER|hO|h %s "
+	CHAT_GUILD_GET = "|Hchannel:GUILD|hG|h %s ",
+	CHAT_OFFICER_GET = "|Hchannel:OFFICER|hO|h %s ",
 	
 	--raid
-	CHAT_RAID_GET = "|Hchannel:RAID|hR|h %s "
-	CHAT_RAID_WARNING_GET = "RW %s "
-	CHAT_RAID_LEADER_GET = "|Hchannel:RAID|hRL|h %s "
+	CHAT_RAID_GET = "|Hchannel:RAID|hR|h %s ",
+	CHAT_RAID_WARNING_GET = "RW %s ",
+	CHAT_RAID_LEADER_GET = "|Hchannel:RAID|hRL|h %s ",
 	
 	--party
-	CHAT_PARTY_GET = "|Hchannel:PARTY|hP|h %s "
-	CHAT_PARTY_LEADER_GET ="|Hchannel:PARTY|hPL|h %s "
-	CHAT_PARTY_GUIDE_GET ="|Hchannel:PARTY|hPG|h %s "
+	CHAT_PARTY_GET = "|Hchannel:PARTY|hP|h %s ",
+	CHAT_PARTY_LEADER_GET ="|Hchannel:PARTY|hPL|h %s ",
+	CHAT_PARTY_GUIDE_GET ="|Hchannel:PARTY|hPG|h %s ",
 	
 	--bg
-	CHAT_BATTLEGROUND_GET = "|Hchannel:BATTLEGROUND|hB|h %s "
-	CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:BATTLEGROUND|hBL|h %s "
+	CHAT_BATTLEGROUND_GET = "|Hchannel:BATTLEGROUND|hB|h %s ",
+	CHAT_BATTLEGROUND_LEADER_GET = "|Hchannel:BATTLEGROUND|hBL|h %s ",
 	
 	--whisper
-	CHAT_WHISPER_INFORM_GET = "to %s "
-	CHAT_WHISPER_GET = "from %s "
-	CHAT_BN_WHISPER_INFORM_GET = "to %s "
-	CHAT_BN_WHISPER_GET = "from %s "
+	CHAT_WHISPER_INFORM_GET = "to %s ",
+	CHAT_WHISPER_GET = "from %s ",
+	CHAT_BN_WHISPER_INFORM_GET = "to %s ",
+	CHAT_BN_WHISPER_GET = "from %s ",
 	
 	--say / yell
-	CHAT_SAY_GET = "%s "
-	CHAT_YELL_GET = "%s "
+	CHAT_SAY_GET = "%s ",
+	CHAT_YELL_GET = "%s ",
 	
 	--flags
-	CHAT_FLAG_AFK = "[AFK] "
-	CHAT_FLAG_DND = "[DND] "
-	CHAT_FLAG_GM = "[GM] "
+	CHAT_FLAG_AFK = "[AFK] ",
+	CHAT_FLAG_DND = "[DND] ",
+	CHAT_FLAG_GM = "[GM] ",
+}
+
+-- 备份
+local BackupChannelReplacement = function()
+	for tag, str in pairs(EditData) do
+		Defaults[tag] = _G[tag]
+	end
+end
+
+-- 刷新
+local UpdateChannelReplacement = function()
+	if aCoreCDB["ChatOptions"]["channelreplacement"] then
+		for tag, str in pairs(EditData) do
+			_G[tag] = str
+		end
+	else
+		for tag, str in pairs(Defaults) do
+			_G[tag] = str
+		end
+	end
+end
+T.UpdateChannelReplacement = UpdateChannelReplacement
+
+EventFrame:SetScript('OnEvent', function(self, event, arg)
+	if arg ~= "AltzUI" then return end
+	
+	BackupChannelReplacement()
+	UpdateChannelReplacement()
 	
 	for i = 1, NUM_CHAT_WINDOWS do
 		if ( i ~= 2 ) then
