14c35b1a7310a6a8766e3b8fe063e03933817563
Paojy <359661355@qq.com>
2024-04-18 19:09:53 +0800

    增加一个api文件

---
+++ Interface/AddOns/AltzUI/core/widget_api.lua
@@ -0,0 +1,11 @@
+﻿local T, C, L, G = unpack(select(2, ...))
+local F = unpack(AuroraClassic)
+
+----------------------------
+-- 			通用		  --
+----------------------------
+
+local function Click(b)
+	local func = b:GetScript("OnMouseDown") or b:GetScript("OnClick")
+	func(b)
+end
