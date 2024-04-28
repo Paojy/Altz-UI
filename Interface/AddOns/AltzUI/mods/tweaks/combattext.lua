3fe1b8787e2e03174c31b633ce0eef20c56169d9
Paojy <359661355@qq.com>
2024-04-29 01:34:20 +0800

    战斗数字选项即时生效

--- Interface/AddOns/AltzUI/mods/tweaks/combattext.lua
+++ Interface/AddOns/AltzUI/mods/tweaks/combattext.lua
@@ -1,7 +1,6 @@
-﻿-- LightCT by Alza
--- xCT by Dandruff
-local T, C, L, G = unpack(select(2, ...))
-local frames = {}
+﻿local T, C, L, G = unpack(select(2, ...))
+
+local iconsize, crit_iconsize = 13, 25
 local dmgcolor = {}
 dmgcolor[1]  = {  1,  1,  0 }  -- physical
 dmgcolor[2]  = {  1, .9, .5 }  -- holy
@@ -12,189 +11,188 @@ dmgcolor[32] = { .5, .5,  1 }  -- shadow
 dmgcolor[64] = {  1, .5,  1 }  -- arcane
 
 local tbl = {
-	["DAMAGE"] = 			{frame = "damagetaken", prefix =  "-", 		arg2 = true, 	r = 1, 		g = 0.1, 	b = 0.1},
-	["DAMAGE_CRIT"] = 		{frame = "damagetaken", prefix = "c-", 		arg2 = true, 	r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_DAMAGE"] = 		{frame = "damagetaken", prefix =  "-", 		arg2 = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
-	["SPELL_DAMAGE_CRIT"] = {frame = "damagetaken", prefix = "c-", 		arg2 = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
-	["HEAL"] = 				{frame = "healingtaken", prefix =  "+", 	arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
-	["HEAL_CRIT"] = 		{frame = "healingtaken", prefix = "c+", 	arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
-	["PERIODIC_HEAL"] = 	{frame = "healingtaken", prefix =  "+", 	arg3 = true, 	r = 0.1, 	g = 1, 		b = 0.1},
-	["MISS"] = 				{frame = "damagetaken", prefix = "Miss", 					r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_MISS"] = 		{frame = "damagetaken", prefix = "Miss", 					r = 0.79, 	g = 0.3, 	b = 0.85},
-	["SPELL_REFLECT"] = 	{frame = "damagetaken", prefix = "Reflect", 				r = 1, 		g = 1, 		b = 1},
-	["DODGE"] = 			{frame = "damagetaken", prefix = "Dodge", 					r = 1, 		g = 0.1, 	b = 0.1},
-	["PARRY"] = 			{frame = "damagetaken", prefix = "Parry", 					r = 1, 		g = 0.1, 	b = 0.1},
-	["BLOCK"] = 			{frame = "damagetaken", prefix = "Block", 	spec = true,	r = 1, 		g = 0.1, 	b = 0.1},
-	["RESIST"] = 			{frame = "damagetaken", prefix = "Resist", 	spec = true, 	r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_RESIST"] = 		{frame = "damagetaken", prefix = "Resist", 	spec = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
-	["ABSORB"] = 			{frame = "damagetaken", prefix = "Absorb", 	spec = true, 	r = 1, 		g = 0.1, 	b = 0.1},
-	["SPELL_ABSORBED"] = 	{frame = "damagetaken", prefix = "Absorb", 	spec = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["DAMAGE"] = 			{frame = "damagetaken",  	prefix =  "-", 		dmg_value = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["DAMAGE_CRIT"] = 		{frame = "damagetaken",  	prefix = "c-", 		dmg_value = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_DAMAGE"] = 		{frame = "damagetaken",  	prefix =  "-", 		dmg_value = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["SPELL_DAMAGE_CRIT"] = {frame = "damagetaken",  	prefix = "c-", 		dmg_value = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["HEAL"] = 				{frame = "healingtaken", 	prefix =  "+", 		heal_value = true, 	r = 0.1, 	g = 1, 		b = 0.1},
+	["HEAL_CRIT"] = 		{frame = "healingtaken", 	prefix = "c+", 		heal_value = true, 	r = 0.1, 	g = 1, 		b = 0.1},
+	["PERIODIC_HEAL"] = 	{frame = "healingtaken", 	prefix =  "+", 		heal_value = true, 	r = 0.1, 	g = 1, 		b = 0.1},
+	["MISS"] = 				{frame = "damagetaken",		prefix = "Miss", 						r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_MISS"] = 		{frame = "damagetaken",		prefix = "Miss", 						r = 0.79, 	g = 0.3, 	b = 0.85},
+	["SPELL_REFLECT"] = 	{frame = "damagetaken",		prefix = "Reflect", 					r = 1, 		g = 1, 		b = 1},
+	["DODGE"] = 			{frame = "damagetaken",		prefix = "Dodge", 						r = 1, 		g = 0.1, 	b = 0.1},
+	["PARRY"] = 			{frame = "damagetaken",		prefix = "Parry", 						r = 1, 		g = 0.1, 	b = 0.1},
+	["BLOCK"] = 			{frame = "damagetaken",		prefix = "Block", 	special = true,		r = 1, 		g = 0.1, 	b = 0.1},
+	["RESIST"] = 			{frame = "damagetaken",		prefix = "Resist", 	special = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_RESIST"] = 		{frame = "damagetaken",		prefix = "Resist", 	special = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
+	["ABSORB"] = 			{frame = "damagetaken",		prefix = "Absorb", 	special = true, 	r = 1, 		g = 0.1, 	b = 0.1},
+	["SPELL_ABSORBED"] = 	{frame = "damagetaken",		prefix = "Absorb", 	special = true, 	r = 0.79, 	g = 0.3, 	b = 0.85},
 }
 
-local GetSpellTextureFormatted = function(spellID, iconSize)
+local frames = CreateFrame("Frame")
+G.CombatText_Frames = frames
+
+local function GetSpellTextureFormatted(spellID, size)
 	local msg = ""
 	if spellID == PET_ATTACK_TEXTURE then
-		msg = " \124T"..PET_ATTACK_TEXTURE..":"..iconSize..":"..iconSize..":0:0:64:64:5:59:5:59\124t"
+		msg = " \124T"..PET_ATTACK_TEXTURE..":"..size..":"..size..":0:0:64:64:5:59:5:59\124t"
 	else
 		local icon = GetSpellTexture(spellID)
 		if icon then
-			msg = " \124T"..icon..":"..iconSize..":"..iconSize..":0:0:64:64:5:59:5:59\124t"
+			msg = " \124T"..icon..":"..size..":"..size..":0:0:64:64:5:59:5:59\124t"
 		else
-			msg = " \124T134400:"..iconSize..":"..iconSize..":0:0:64:64:5:59:5:59\124t"
+			msg = " \124T134400:"..size..":"..size..":0:0:64:64:5:59:5:59\124t"
 		end
 	end
 	return msg
 end
 
 local function CreateCTFrame(i, movingname, justify, a1, parent, a2, x, y)
-	local f = CreateFrame("ScrollingMessageFrame", "Combat Text"..i, UIParent)
+	local frame = CreateFrame("ScrollingMessageFrame", "Combat Text"..i, UIParent)
 	
-	f:SetFont("Interface\\AddOns\\AltzUI\\media\\number.ttf", aCoreCDB["CombattextOptions"]["cticonsize"], "OUTLINE")
-	f:SetShadowColor(0, 0, 0, 0)
-	f:SetFadeDuration(0.2)
-	f:SetTimeVisible(aCoreCDB["CombattextOptions"]["ctfadetime"])
-	f:SetMaxLines(20)
-	f:SetSpacing(3)
-	f:SetWidth(84)
-	f:SetHeight(150)
+	frame:SetFont("Interface\\AddOns\\AltzUI\\media\\number.ttf", iconsize, "OUTLINE")
+	frame:SetJustifyH(justify)
 	
-	f.movingname = movingname
-	f.point = {
+	frame:SetShadowColor(0, 0, 0, 0)
+	frame:SetFadeDuration(0.2)
+	frame:SetTimeVisible(3)
+	frame:SetMaxLines(20)
+	frame:SetSpacing(3)
+	frame:SetWidth(84)
+	frame:SetHeight(150)
+	
+	frame.movingname = movingname
+	frame.point = {
 		healer = {a1 = a1, parent = parent:GetName(), a2 = a2, x = x, y = y},
 		dpser = {a1 = a1, parent = parent:GetName(), a2 = a2, x = x, y = y},
 	}
-	T.CreateDragFrame(f)
+	T.CreateDragFrame(frame)
 	
-	f:SetJustifyH(justify)
-	
-	f.df:SetScript("OnUpdate", function(self, elapsed)
+	frame.df:SetScript("OnUpdate", function(self, elapsed)
 		self.timer = (self.timer or 0) + elapsed
 		if self.timer > 1 then	
 			self.number = random(1 , 10000)
 			if aCoreCDB["CombattextOptions"]["showreceivedct"] then
-				frames["damagetaken"]:AddMessage("-"..self.number, 1, 0, 0)
-				frames["healingtaken"]:AddMessage("+"..self.number, 0, 1, 0)
+				frames.damagetaken:AddMessage("-"..self.number, 1, 0, 0)
+				frames.healingtaken:AddMessage("+"..self.number, 0, 1, 0)
 			end
 			if aCoreCDB["CombattextOptions"]["showoutputct"] then
-				frames["outputdamage"]:AddMessage(GetSpellTextureFormatted(6603, aCoreCDB["CombattextOptions"]["cticonsize"])..self.number, 1, 1, 0)
-				frames["outputhealing"]:AddMessage(GetSpellTextureFormatted(139, aCoreCDB["CombattextOptions"]["cticonsize"])..self.number, 0.3, 1, 0)
+				frames.outputdamage:AddMessage(GetSpellTextureFormatted(6603, iconsize)..self.number, 1, 1, 0)
+				frames.outputhealing:AddMessage(GetSpellTextureFormatted(139, iconsize)..self.number, 0.3, 1, 0)
 			end
 			self.timer = 0
 		end
 	end)
 	
-	return f
+	return frame
 end
 
-local eventframe = CreateFrame("Frame")
-eventframe:SetScript("OnEvent", function(self, event, ...)
-	if not aCoreCDB["CombattextOptions"]["combattext"] then return end
-	self[event](self, ...)
-end)
-
-function eventframe:COMBAT_TEXT_UPDATE(spelltype)
-	local info = tbl[spelltype]
-	if info then
-		local msg = info.prefix
-		local arg2, arg3 = GetCurrentCombatTextEventInfo()
-		if info.spec  then
-			if arg3 then
-				msg = string.format("-%s (%s)", arg2, arg3)
-			end
-		else
-			if info.arg2 and arg2 then msg = msg..T.ShortValue(arg2) end
-			if info.arg3 and arg3 then msg = msg..T.ShortValue(arg3) end
-		end
-		frames[info.frame]:AddMessage(msg, info.r, info.g, info.b)
-	end
-end
-
-function eventframe:COMBAT_LOG_EVENT_UNFILTERED()
-	local icon, spellId, amount, critical, spellSchool
-    local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destFlags2, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21 = CombatLogGetCurrentEventInfo()
-	if sourceGUID == UnitGUID("player") or (aCoreCDB["CombattextOptions"]["ctshowpet"] and sourceGUID == UnitGUID("pet")) or sourceFlags == gflags then
-		if eventType == 'SPELL_HEAL' or (aCoreCDB["CombattextOptions"]["ctshowhots"] and eventType == 'SPELL_PERIODIC_HEAL') then
-			spellId = arg12
-			amount = arg15
-			critical = arg18
-			icon = GetSpellTextureFormatted(spellId, critical and aCoreCDB["CombattextOptions"]["ctbigiconsize"] or aCoreCDB["CombattextOptions"]["cticonsize"])
-			frames["outputhealing"]:AddMessage(T.ShortValue(amount)..""..icon, 0, 1, 0)
-		elseif destGUID ~= UnitGUID("player") then
-			if eventType=="SWING_DAMAGE" then
-				amount = arg12
-				critical = arg18
-				icon = GetSpellTextureFormatted(6603, critical and aCoreCDB["CombattextOptions"]["ctbigiconsize"] or aCoreCDB["CombattextOptions"]["cticonsize"])
-			elseif eventType == "RANGE_DAMAGE" then
-				spellId = arg12
-				amount = arg15
-				critical = arg21
-				icon = GetSpellTextureFormatted(spellId, critical and aCoreCDB["CombattextOptions"]["ctbigiconsize"] or aCoreCDB["CombattextOptions"]["cticonsize"])
-			elseif eventType == "SPELL_DAMAGE" or (aCoreCDB["CombattextOptions"]["ctshowdots"] and eventType == "SPELL_PERIODIC_DAMAGE") then
-				spellId = arg12
-				spellSchool = arg14
-				amount = arg15
-				critical = arg21
-				icon = GetSpellTextureFormatted(spellId, critical and aCoreCDB["CombattextOptions"]["ctbigiconsize"] or aCoreCDB["CombattextOptions"]["cticonsize"])
-			elseif eventType == "SWING_MISSED" then
-				amount = arg12 -- misstype
-				icon = GetSpellTextureFormatted(6603, aCoreCDB["CombattextOptions"]["cticonsize"])
-			elseif eventType == "SPELL_MISSED" or eventType == "RANGE_MISSED" then
-				spellId = arg12
-				amount = arg15 -- misstype
-				icon = GetSpellTextureFormatted(spellId, aCoreCDB["CombattextOptions"]["cticonsize"])
+frames:SetScript("OnEvent", function(self, event, ...)
+	if event == "COMBAT_TEXT_UPDATE" then
+		local spelltype = ...
+		local info = tbl[spelltype]
+		if info then
+			local arg2, arg3 = GetCurrentCombatTextEventInfo()
+			local msg
+			
+			if info.special and arg3 then
+				msg = info.prefix..string.format(" %s (%s)", arg2, arg3)
+			elseif info.dmg_value and arg2 then
+				msg = info.prefix..T.ShortValue(arg2)
+			elseif info.heal_value and arg3 then
+				msg = info.prefix..T.ShortValue(arg3)
+			else
+				msg = info.prefix
 			end
 			
-			if amount and icon then
-				if critical then
-					if dmgcolor[spellSchool] then
-						frames["outputdamage"]:AddMessage("+"..T.ShortValue(amount)..""..icon, dmgcolor[spellSchool][1], dmgcolor[spellSchool][2], dmgcolor[spellSchool][3])
+			frames[info.frame]:AddMessage(msg, info.r, info.g, info.b)
+		end
+	elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
+		local icon, spellId, amount, critical, spellSchool
+		local timestamp, eventType, hideCaster, sourceGUID, sourceName, sourceFlags, sourceFlags2, destGUID, destName, destFlags, destFlags2, arg12, arg13, arg14, arg15, arg16, arg17, arg18, arg19, arg20, arg21 = CombatLogGetCurrentEventInfo()
+		if sourceGUID == UnitGUID("player") or (aCoreCDB["CombattextOptions"]["ctshowpet"] and sourceGUID == UnitGUID("pet")) then
+			if eventType == 'SPELL_HEAL' or (aCoreCDB["CombattextOptions"]["ctshowhots"] and eventType == 'SPELL_PERIODIC_HEAL') then
+				spellId = arg12
+				amount = arg15
+				critical = arg18
+				icon = GetSpellTextureFormatted(spellId, critical and crit_iconsize or iconsize)
+				frames.outputhealing:AddMessage(T.ShortValue(amount)..""..icon, 0, 1, 0)
+			elseif destGUID ~= UnitGUID("player") then
+				if eventType=="SWING_DAMAGE" then
+					amount = arg12
+					critical = arg18
+					icon = GetSpellTextureFormatted(6603, critical and crit_iconsize or iconsize)
+				elseif eventType == "RANGE_DAMAGE" then
+					spellId = arg12
+					amount = arg15
+					critical = arg21
+					icon = GetSpellTextureFormatted(spellId, critical and crit_iconsize or iconsize)
+				elseif eventType == "SPELL_DAMAGE" or (aCoreCDB["CombattextOptions"]["ctshowdots"] and eventType == "SPELL_PERIODIC_DAMAGE") then
+					spellId = arg12
+					spellSchool = arg14
+					amount = arg15
+					critical = arg21
+					icon = GetSpellTextureFormatted(spellId, critical and crit_iconsize or iconsize)
+				elseif eventType == "SWING_MISSED" then
+					amount = arg12 -- misstype
+					icon = GetSpellTextureFormatted(6603, iconsize)
+				elseif eventType == "SPELL_MISSED" or eventType == "RANGE_MISSED" then
+					spellId = arg12
+					amount = arg15 -- misstype
+					icon = GetSpellTextureFormatted(spellId, iconsize)
+				end
+				
+				if amount and icon then
+					if critical then
+						if dmgcolor[spellSchool] then
+							frames.outputdamage:AddMessage("+"..T.ShortValue(amount)..""..icon, dmgcolor[spellSchool][1], dmgcolor[spellSchool][2], dmgcolor[spellSchool][3])
+						else
+							frames.outputdamage:AddMessage("+"..T.ShortValue(amount)..""..icon, dmgcolor[1][1], dmgcolor[1][2], dmgcolor[1][3])
+						end
 					else
-						frames["outputdamage"]:AddMessage("+"..T.ShortValue(amount)..""..icon, dmgcolor[1][1], dmgcolor[1][2], dmgcolor[1][3])
-					end
-				else
-					if dmgcolor[spellSchool] then
-						frames["outputdamage"]:AddMessage(T.ShortValue(amount)..""..icon, dmgcolor[spellSchool][1], dmgcolor[spellSchool][2], dmgcolor[spellSchool][3])
-					else
-						frames["outputdamage"]:AddMessage(T.ShortValue(amount)..""..icon, dmgcolor[1][1], dmgcolor[1][2], dmgcolor[1][3])
+						if dmgcolor[spellSchool] then
+							frames.outputdamage:AddMessage(T.ShortValue(amount)..""..icon, dmgcolor[spellSchool][1], dmgcolor[spellSchool][2], dmgcolor[spellSchool][3])
+						else
+							frames.outputdamage:AddMessage(T.ShortValue(amount)..""..icon, dmgcolor[1][1], dmgcolor[1][2], dmgcolor[1][3])
+						end
 					end
 				end
 			end
 		end
 	end
-end
+end)
 
-local function Init()
-	SetCVar("floatingCombatTextCombatDamage", 1)
-	SetCVar("floatingCombatTextCombatHealing", 1)
-	
-	if aCoreCDB["CombattextOptions"]["hidblz_receive"] then
-		SetCVar("floatingCombatTextCombatDamage", 0)
-		SetCVar("floatingCombatTextCombatHealing", 0)
-	end
-	
-	if aCoreCDB["CombattextOptions"]["hidblz"] then
-		SetCVar("enableFloatingCombatText", 0)
-	else
-		SetCVar("enableFloatingCombatText", 1)
-	end
+T.RegisterInitCallback(function()
+	frames.damagetaken = CreateCTFrame("damagetaken", L["承受伤害"], "LEFT", "RIGHT", UIParent, "CENTER", -485, 0)
+	frames.healingtaken = CreateCTFrame("healingtaken", L["承受治疗"], "RIGHT", "LEFT", UIParent, "CENTER", -665, 0)
 	
 	if aCoreCDB["CombattextOptions"]["showreceivedct"] then
-		frames["damagetaken"] = CreateCTFrame("damagetaken", L["承受伤害"], "LEFT", "RIGHT", UIParent, "CENTER", -485, 0)
-		frames["healingtaken"] = CreateCTFrame("healingtaken", L["承受治疗"], "RIGHT", "LEFT", UIParent, "CENTER", -665, 0)
-		eventframe:RegisterEvent("COMBAT_TEXT_UPDATE")
+		T.RestoreDragFrame(frames.damagetaken)
+		T.RestoreDragFrame(frames.healingtaken)
+		frames:RegisterEvent("COMBAT_TEXT_UPDATE")
+	else
+		T.ReleaseDragFrame(frames.damagetaken)
+		T.RestoreDragFrame(frames.healingtaken)
+		frames:RegisterEvent("COMBAT_TEXT_UPDATE")
 	end
 	
+	frames.outputdamage = CreateCTFrame("outputdamage", L["输出伤害"], "RIGHT", "LEFT", UIParent, "CENTER", 485, 80)
+	frames.outputhealing = CreateCTFrame("outputhealing", L["输出治疗"], "LEFT", "RIGHT", UIParent, "CENTER", 665, 80)	
+	
 	if aCoreCDB["CombattextOptions"]["showoutputct"] then
-		frames["outputdamage"] = CreateCTFrame("outputdamage", L["输出伤害"], "RIGHT", "LEFT", UIParent, "CENTER", 485, 80)
-		frames["outputhealing"] = CreateCTFrame("outputhealing", L["输出治疗"], "LEFT", "RIGHT", UIParent, "CENTER", 665, 80)	
-		eventframe:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+		T.RestoreDragFrame(frames.outputdamage)
+		T.RestoreDragFrame(frames.outputhealing)
+		frames:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+	else
+		T.ReleaseDragFrame(frames.outputdamage)
+		T.RestoreDragFrame(frames.outputhealing)
+		frames:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 	end
 	
 	-- 战斗文字字体
-	local font = aCoreCDB["SkinOptions"]["combattext"]
+	local font = aCoreCDB["CombattextOptions"]["combattext_font"]
 	if font ~= "none" then
 		DAMAGE_TEXT_FONT = G.combatFont[font]
 	end
-end
-
-T.RegisterInitCallback(Init)
+end)
