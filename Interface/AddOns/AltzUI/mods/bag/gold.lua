06de0335f9fe86c20bf304daa58c389136e307fa
Paojy <359661355@qq.com>
2024-05-03 01:54:18 +0800

    重写角色金币统计

--- Interface/AddOns/AltzUI/mods/bag/gold.lua
+++ Interface/AddOns/AltzUI/mods/bag/gold.lua
@@ -2,6 +2,10 @@ local T, C, L, G = unpack(select(2, ...))
 
 local Profit, Spent, OldMoney = 0, 0, 0
 
+--====================================================--
+--[[                   -- API --                    ]]--
+--====================================================--
+
 local function formatMoney(money)
 	local gold = floor(math.abs(money) / 10000)
 	local silver = mod(floor(math.abs(money) / 100), 100)
@@ -20,22 +24,17 @@ local function FormatTooltipMoney(money)
 	local cash = ""
 	cash = format("%d".."|cffffd700g|r".." %d".."|cffc7c7cfs|r".." %d".."|cffeda55fc|r", gold, silver, copper)		
 	return cash
-end	
-	
+end
+
+--====================================================--
+--[[                -- Update --                    ]]--
+--====================================================--
 local eventframe = CreateFrame("Frame")
 
 eventframe:SetScript("OnEvent", function(self, event)
-	if event == "PLAYER_ENTERING_WORLD" then
-		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
-		OldMoney = GetMoney()
-		if aCoreDB.gold[G.PlayerRealm] == nil then 
-			aCoreDB.gold[G.PlayerRealm] = {}
-		end	
-	end
-		
 	local NewMoney	= GetMoney()
 	local Change = NewMoney - OldMoney -- Positive if we gain money
-		
+	
 	if OldMoney > NewMoney then		-- Lost Money
 		Spent = Spent - Change
 	else							-- Gained Moeny
@@ -43,7 +42,6 @@ eventframe:SetScript("OnEvent", function(self, event)
 	end
 	
 	OldMoney = NewMoney
-
 	aCoreDB.gold[G.PlayerRealm][G.PlayerName] = GetMoney()
 end)
 
@@ -52,24 +50,11 @@ eventframe:RegisterEvent("SEND_MAIL_MONEY_CHANGED")
 eventframe:RegisterEvent("SEND_MAIL_COD_CHANGED")
 eventframe:RegisterEvent("PLAYER_TRADE_MONEY")
 eventframe:RegisterEvent("TRADE_MONEY_CHANGED")
-eventframe:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 local Gold = ContainerFrameCombinedBags.MoneyFrame
 
-local ResetButton = CreateFrame("Button", "Reset_Statistics", Gold)
-ResetButton:SetSize(15, 15)
-ResetButton:SetPoint("LEFT", Gold, "RIGHT", 55, 0)
-ResetButton:SetFrameLevel(Gold:GetFrameLevel()+2)
-ResetButton:SetAlpha(0)
-
-ResetButton.tex = ResetButton:CreateTexture(nil, "ARTWORK")
-ResetButton.tex:SetAllPoints()
-ResetButton.tex:SetTexture("Interface\\Buttons\\UI-RefreshButton")
-ResetButton:RegisterForClicks("AnyDown")
-
 local function ShowMoneyTooltip()
 	GameTooltip:SetOwner(Gold, "ANCHOR_TOPLEFT")
-	GameTooltip:SetPoint("BOTTOMLEFT", _G[G.uiname.."bag"], "TOPLEFT", -1, 2)
 	GameTooltip:AddLine(L["本次登陆"]..": ", unpack(G.addon_color))
 	GameTooltip:AddDoubleLine(L["赚得"], formatMoney(Profit), 1, 1, 1, 1, 1, 1)
 	GameTooltip:AddDoubleLine(L["消费"], formatMoney(Spent), 1, 1, 1, 1, 1, 1)
@@ -88,43 +73,39 @@ local function ShowMoneyTooltip()
 	GameTooltip:AddLine(" ")
 	GameTooltip:AddLine(L["服务器"]..": ", unpack(G.addon_color))
 	GameTooltip:AddDoubleLine(TOTAL..": ", FormatTooltipMoney(totalGold), 1, 1, 1, 1, 1, 1)
-	for i = 1, 3 do
-		local name, count, extraCurrencyType, icon, itemID = C_CurrencyInfo.GetBackpackCurrencyInfo(i)
-		if name and i == 1 then
-			GameTooltip:AddLine(" ")
-			GameTooltip:AddLine(CURRENCY,unpack(G.addon_color))
-		end
-		local r, g, b = 1 ,1 ,1
-		if itemID then r, g, b = GetItemQualityColor(select(3, GetItemInfo(itemID))) end
-		if name and count then GameTooltip:AddDoubleLine(name, count, 1, 1, 1, 1, 1, 1) end
-	end
+	
 	GameTooltip:Show()
-	ResetButton:SetAlpha(1)
 end
 
-Gold:SetScript("OnEnter", function() ShowMoneyTooltip() end)
-Gold:SetScript("OnLeave", function() GameTooltip:Hide() end)
-
-for i, child in ipairs({Gold:GetChildren()}) do
-	child:SetScript("OnEnter", function() ShowMoneyTooltip() end)
-	child:SetScript("OnLeave", function() GameTooltip:Hide() ResetButton:SetAlpha(0) end)
+local function HookTooltip(frame)
+	frame:SetScript("OnEnter", function() ShowMoneyTooltip() end)
+	frame:SetScript("OnLeave", function() GameTooltip:Hide() end)
 end
+
+local buttons = {Gold, Gold.CopperButton, Gold.SilverButton, Gold.GoldButton}
+
+for i, bu in ipairs(buttons) do
+	HookTooltip(bu)
+end
+
+local ResetButton = T.ClickTexButton(Gold, {"RIGHT", Gold, "RIGHT", 0, 0}, [[Interface\AddOns\AltzUI\media\icons\refresh.tga]], nil, nil, L["重置金币信息"])
 
 ResetButton:SetScript("OnClick", function(self)
-	aCoreDB.gold[G.PlayerRealm] = {}
+	aCoreDB.gold[G.PlayerRealm] = table.wipe(aCoreDB.gold[G.PlayerRealm])
 	aCoreDB.gold[G.PlayerRealm][G.PlayerName] = GetMoney()
 	ShowMoneyTooltip()
 end)
 
-ResetButton:SetScript("OnEnter", function(self)
-	GameTooltip:SetOwner(self, "ANCHOR_NONE")
-	GameTooltip:SetPoint("BOTTOMLEFT", _G[G.uiname.."bag"], "TOPLEFT", -1, 2)
-	GameTooltip:AddLine(L["重置金币信息"])
-	GameTooltip:Show()
-	self:SetAlpha(1)
+hooksecurefunc("MoneyFrame_Update", function()
+	Gold.CopperButton:ClearAllPoints()
+	Gold.CopperButton:SetPoint("RIGHT", Gold, "RIGHT", -23, 0)
 end)
 
-ResetButton:SetScript("OnLeave", function(self)
-	GameTooltip:Hide()
-	self:SetAlpha(0)
+T.RegisterEnteringWorldCallback(function()
+	OldMoney = GetMoney()
+	if aCoreDB.gold[G.PlayerRealm] == nil then 
+		aCoreDB.gold[G.PlayerRealm] = {}
+	end
+	MONEY_TEXT_VADJUST = 0
 end)
+
