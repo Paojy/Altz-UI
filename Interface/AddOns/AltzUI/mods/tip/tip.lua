35f1b2c536a00accdc68678b9902e9a85b8718d8
Paojy <359661355@qq.com>
2024-05-09 08:46:04 +0800

    修鼠标提示高占用bug

--- Interface/AddOns/AltzUI/mods/tip/tip.lua
+++ Interface/AddOns/AltzUI/mods/tip/tip.lua
@@ -1,33 +1,37 @@
 ﻿local T, C, L, G = unpack(select(2, ...))
 
+local EnumItem = Enum.TooltipDataType.Item
+local EnumSpell = Enum.TooltipDataType.Spell
+local EnumAura = Enum.TooltipDataType.UnitAura
+local EnumUnit = Enum.TooltipDataType.Unit
+
 hooksecurefunc(GameTooltip, "ProcessLines", function(self)
-	local tooltip = self or GameTooltip
-	local tooltipData = tooltip:GetPrimaryTooltipData()
+	local tooltipData = self:GetPrimaryTooltipData()
 	if tooltipData then
 		if aCoreCDB["OtherOptions"]["combathide"] and InCombatLockdown() then
-			 tooltip:Hide()
+			 self:Hide()
 			 return
 		end
-		if tooltipData.type == Enum.TooltipDataType.Item then	
+		if tooltipData.type == EnumItem then	
 			if aCoreCDB["OtherOptions"]["show_itemID"] then
 				local itemID = tooltipData.id
 				if itemID then
-					tooltip:AddLine(" ")
-					tooltip:AddDoubleLine(T.color_text(L["物品编号"]), T.hex_str(itemID, 1, 1, 1))
-					tooltip:Show()
+					self:AddLine(" ")
+					self:AddDoubleLine(T.color_text(L["物品编号"]), T.hex_str(itemID, 1, 1, 1))
+					self:Show()
 				end
 			end
-		elseif (tooltipData.type == Enum.TooltipDataType.Spell or tooltipData.type == Enum.TooltipDataType.UnitAura) then
+		elseif (tooltipData.type == EnumSpell or tooltipData.type == EnumAura) then
 			if aCoreCDB["OtherOptions"]["show_spellID"] then
 				local spellID = tooltipData.id
 				if spellID then
-					tooltip:AddLine(" ")
-					tooltip:AddDoubleLine(T.color_text(L["法术编号"]), T.hex_str(spellID, 1, 1, 1))
-					tooltip:Show()
+					self:AddLine(" ")
+					self:AddDoubleLine(T.color_text(L["法术编号"]), T.hex_str(spellID, 1, 1, 1))
+					self:Show()
 				end
 			end
-		elseif tooltipData.type == Enum.TooltipDataType.Unit then
-			local _, unit = tooltip:GetUnit()
+		elseif tooltipData.type == EnumUnit then
+			local _, unit = self:GetUnit()
 			if unit then
 				local r, g, b = T.GetUnitColor(unit)
 				
@@ -39,7 +43,7 @@ hooksecurefunc(GameTooltip, "ProcessLines", function(self)
 			end
 		end
 		
-		for i= 1, tooltip:NumLines() do
+		for i= 1, self:NumLines() do
 			local tiptext_left = _G["GameTooltipTextLeft"..i]
 			local tiptext_right = _G["GameTooltipTextRight"..i]
 			if tiptext_left then
@@ -49,6 +53,8 @@ hooksecurefunc(GameTooltip, "ProcessLines", function(self)
 				tiptext_right:SetFont(G.norFont, 12, "OUTLINE")
 			end
 		end
+		
+		tooltipData = table.wipe(tooltipData) -- 避免高占用
 	end
 end)
 
