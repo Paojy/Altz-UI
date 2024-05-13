789b99e1add89fa0abd18c589c01e2332b7c55ac
Paojy <359661355@qq.com>
2024-05-13 19:18:48 +0800

    更改字体路径

--- Interface/AddOns/!AltzUIFont/core.lua
+++ Interface/AddOns/!AltzUIFont/core.lua
@@ -3,6 +3,6 @@ f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(self, event, addon)	
 	if addon == "AuroraClassic" then
 		local _, _, _, C = unpack(AuroraClassic)
-		C.Font[1] =  "Interface\\AddOns\\AltzUI\\media\\font.ttf" -- 字体路径
+		C.Font[1] =  "Interface\\AddOns\\AltzUI\\media\\fonts\\font.ttf" -- 字体路径
 	end
 end)
