93a65c06ae343139be9d5d457e77240337cb50c0
Paojy <359661355@qq.com>
2024-08-19 12:34:48 +0800

    修改任务追踪栏字体

--- Interface/AddOns/AltzUI/mods/tweaks/watchframe.lua
+++ Interface/AddOns/AltzUI/mods/tweaks/watchframe.lua
@@ -1,6 +1,15 @@
 --Original Author: Nibelheim
 local T, C, L, G = unpack(select(2, ...))
 
+
+local function ReskinFont(font, size)
+	if not font then return end
+	local oldSize = select(2, font:GetFont())
+	size = size or oldSize
+	font:SetFont(G.norFont, size, "OUTLINE")
+	font:SetShadowColor(0, 0, 0, 0)
+end
+
 local eventframe = CreateFrame("Frame")
 eventframe:RegisterEvent("PLAYER_ENTERING_WORLD")
 eventframe:SetScript("OnEvent", function()
@@ -11,4 +20,9 @@ eventframe:SetScript("OnEvent", function()
 			ObjectiveTrackerFrame.Header:SetCollapsed(true)
 		end
 	end
+	
+	ReskinFont(ObjectiveTrackerLineFont)
+	ReskinFont(ObjectiveTrackerHeaderFont)
 end)
+
+
